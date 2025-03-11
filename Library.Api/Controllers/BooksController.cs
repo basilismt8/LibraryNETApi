e438c14751c0398e4bd4567b56dc7243e6cb6c2d
Vasileios Mastoras <vmastoras@terracom.gr>
2025-03-11 15:50:31 +0200

    book controler added. also dto for the book domain is added

---
+++ Library.Api/Controllers/BooksController.cs
@@ -0,0 +1,58 @@
+﻿using Library.Api.Data;
+using Library.Api.Models.Domain;
+using Library.Api.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly LibraryDbContext dbContext;
+
+        public BooksController(LibraryDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet("getAll")]
+        public IActionResult getAll() {
+            var booksDomain = dbContext.Books.ToList();
+            var booksDto = new List<BookDto>();
+
+            foreach (var bookDomain in booksDomain){
+                booksDto.Add(new BookDto() {
+                    id = bookDomain.id,
+                    title = bookDomain.title,
+                    copiesAvailable = bookDomain.copiesAvailable,
+                    totalCopies = bookDomain.totalCopies
+                });
+            }
+
+            return Ok(booksDto);
+        }
+
+        [HttpGet("getById/{id}")]
+        public IActionResult getById(Guid id)
+        {
+            var bookDomain = dbContext.Books.Find(id);
+            //LINQ query. Is the same with the above line!!!
+            //var book = dbContext.Books.FirstOrDefault(x => x.id == id);
+
+            if (bookDomain == null) {
+                return NotFound();
+            }
+
+            var bookDto = new BookDto {
+                id = bookDomain.id,
+                title = bookDomain.title,
+                copiesAvailable = bookDomain.copiesAvailable,
+                totalCopies = bookDomain.totalCopies
+            };
+
+            return Ok(bookDto);
+        }
+    }
+}
---
+++ Library.Api/Models/Dto/BookDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Api.Models.Dto
+{
+    public class BookDto
+    {
+
+        public Guid id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public int copiesAvailable { get; set; }
+        public int totalCopies { get; set; }
+    }
+}
