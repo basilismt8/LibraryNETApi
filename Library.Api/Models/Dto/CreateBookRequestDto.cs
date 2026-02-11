0b24795b52baca67cb44329643311b6e11958b9f
Vasileios Mastoras <basilismt8@gmail.com>
2026-02-11 14:37:31 +0200

    Added validation for copies available can't be greater than total copies

--- Library.Api/Models/Dto/CreateBookRequestDto.cs
+++ Library.Api/Models/Dto/CreateBookRequestDto.cs
@@ -2,15 +2,24 @@
 
 namespace Library.Api.Models.Dto
 {
-    public class CreateBookRequestDto
+    public class CreateBookRequestDto : IValidatableObject
     {
         [Required]
-        [MinLength(10, ErrorMessage = "Title must have a minimum 30 characters")]
+        [MinLength(10, ErrorMessage = "Title must have a minimum 10 characters")]
         [MaxLength(255, ErrorMessage = "Title must have a maximum 255 characters")]
         public string title { get; set; } = string.Empty;
         [Range(1, int.MaxValue, ErrorMessage = "Copies Available must be at least 1.")]
         public int copiesAvailable { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Total Copies must be at least 1.")]
         public int totalCopies { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (copiesAvailable > totalCopies)
+            {
+                yield return new ValidationResult(
+                    "Copies Available cannot be greater than Total Copies.",
+                    new[] { nameof(copiesAvailable) });
+            }
+        }
     }
 }
--- Library.Api/Models/Dto/UpdateBookRequestDto.cs
+++ Library.Api/Models/Dto/UpdateBookRequestDto.cs
@@ -2,15 +2,24 @@
 
 namespace Library.Api.Models.Dto
 {
-    public class UpdateBookRequestDto
+    public class UpdateBookRequestDto : IValidatableObject
     {
         [Required]
-        [MinLength(30, ErrorMessage = "Title must have a minimum 30 characters")]
+        [MinLength(10, ErrorMessage = "Title must have a minimum 10 characters")]
         [MaxLength(255, ErrorMessage = "Title must have a maximum 255 characters")]
         public string title { get; set; } = string.Empty;
         [Range(1, int.MaxValue, ErrorMessage = "copiesAvailable must be at least 1.")]
         public int copiesAvailable { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "totalCopies must be at least 1.")]
         public int totalCopies { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (copiesAvailable > totalCopies)
+            {
+                yield return new ValidationResult(
+                    "Copies Available cannot be greater than Total Copies.",
+                    new[] { nameof(copiesAvailable) });
+            }
+        }
     }
 }
